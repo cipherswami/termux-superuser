dd365d072835d0d28c33c4ae714fd34c599b3e1a
Aravind Swami <aravindswami135@gmail.com>
2021-09-13 01:28:18 +0530

    testing dev1

--- install_xsu.sh
+++ install_xsu.sh
@@ -1,7 +1,43 @@
 #! /data/data/com.termux/files/usr/bin/bash
 
 # This is Termux supreruser installer,
-# you can delete this file safely after installation.
+# you can safely delete this file after installation.
+
+# This installer downloads my addtional libraries,
+# for installation.
+
+# For the source code of installation libraries,
+# you can visit: https://github.com/name-is-cipher/cipherus-termux/blob/main/cipherus-libraries.sh
+# or there is copy of libraries file in assets folder ( Might not be updated version ).
+
+if [ ! -d ~/.termux ]; then
+
+    clear
+    echo " "
+    echo " [!] Your are on older version of Termux !!!"
+    echo "     Please update and re run the Installation"
+    exit
+
+fi
+
+clear
+
+## Pre variable initiations ##
+Dev=1
+
+if [[ Dev == 0 ]]; then
+    export BRANCH=main
+else
+    export BRANCH=development
+fi
+
+###   Downloading installation Libraries   ###
+
+if [[ ! -f cipherus-libraries.sh ]]; then
+curl -Os https://raw.githubusercontent.com/name-is-cipher/cipherus-termux/$BRANCH/cipherus-libraries.sh
+fi
+
+source cipherus-libraries.sh
 
 function banner_termux-superuser() {
 
@@ -21,100 +57,18 @@ function banner_termux-superuser() {
     echo "  "
 }
 
-
-
-function check_tbin() {
-
-    if [ ! -d ~/.termux/bin ]; then
-
-        mkdir ~/.termux/bin
-        echo >> ~/.bashrc
-        echo "# This PATH is for Termux superuser bin folder" >> ~/.bashrc
-        echo >> ~/.bashrc
-        echo "export PATH=\$PATH:/data/data/com.termux/files/home/.termux/bin" >> ~/.bashrc
-
-    fi
-
-}
-
-function clean_cipherus() {
-
-    if [ -f cipherus-libraries.sh ]; then
-        rm cipherus-libraries.sh
-        rm ~/.wget-hsts
-    fi
-}
-
-function ibar {
-
-    FILE=$1
-    BAR='##############################'
-    FILL='------------------------------'
-    Lines=$2  # To No. lines in file that need to be present.
-    barLen=30 # Bar Lenght of progressbar.
-    count=0
-
-    echo " "
-    # --- iterate over lines in of passed on file ---
-    while IFS=, read -r line; do
-    # update progress bar
-    sleep 0.1
-    count=$(($count + 1))
-    percent=$((($count * 100 / $Lines * 100) / 100))
-    i=$(($percent * $barLen / 100))
-    echo -ne "\r[${BAR:0:$i}${FILL:$i:barLen}] $count/$Lines ($percent %)"
-    done < $FILE
-    echo " "
-
-    # Integrity checker
-    if [ $percent != 100 ]; then
-    echo " "
-    echo " [!] File is corrupt, Please try to reinstall !!!"
-    echo " "
-    echo " If you keep seeing this error, contact the Author:-"
-    echo " "
-    echo " github: name-is-cipher"
-    echo " Twitter: name_is_cipher"
-    echo " Mail: aravindswami135@gmail.com"
-    clean_cipherus
-    read
-    exit
-    fi
-    echo " "
-    echo " "
-
-}
-
-function install_termux-superuser() {
-
-    echo " [*] Installing Termux superuser ..."
-    echo " "
-    
-    chmod +x ~/.termux/bin/xsu
-    ibar ~/.termux/bin/xsu 12
-
-    echo " "
-    echo " [*] Installation successful !!!"
-    echo " "
-    echo "> Run 'xsu' anywhere to start Termux Superuser."
-    echo " "
-    echo " [*] Termux needs to be restarted to work properly,"
-    echo "     Please restart !"
-    echo " "
-    read
-    exit
-
-}
-
-
 ################# Main ################
 
 banner_termux-superuser
-
-check_update
 
 check_tbin
 
 install_termux-superuser
 
-#####################################
+echo " [*] Termux needs to be restarted to work properly,"
+echo "     Please restart !"
+echo " "
+read
+exit
+
+############### THE END #################
